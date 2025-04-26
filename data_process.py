c9b29632f991bece60611bba10e4ced09401fd5f
zhongweiwan <wan.512@buckeyemail.osu.edu>
2025-04-26 16:56:26 +0000

    refactor: Merge data preprocessing procedure together

--- data_process.py
+++ data_process.py
@@ -1,5 +1,21 @@
 import json
 import os
+import pandas as pd
+import json
+import numpy as np
+
+# 加载 parquet 文件
+df = pd.read_parquet("39Krelease.parquet") #替换真实位置，同下
+
+# 把所有 numpy 类型转为原生 Python 类型（如 list、float 等）
+for col in df.columns:
+    df[col] = df[col].map(lambda x: x.tolist() if isinstance(x, np.ndarray) else x)
+
+# 保存为 jsonl
+with open("39Krelease2.jsonl", "w", encoding="utf-8") as f:
+    for record in df.to_dict(orient="records"):
+        f.write(json.dumps(record, ensure_ascii=False) + "\n")
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -30,6 +46,5 @@ with open("39Krelease2.jsonl", "r", encoding="utf-8") as input_file, open("39Kre
             "answer": answer
         }
 
-        # 写入文件
         json.dump(formatted, output_file, ensure_ascii=False)
         output_file.write("\n")
